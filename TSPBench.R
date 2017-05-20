7ed0ade1320bd0ec0e111e37233b23960cc0eaa4
Guilhem Piat <guilhempiat@yahoo.fr>
2017-05-20 21:50:03 +0200

    + Created k-opt function [UNTESTED]
    ~ fixed 2-opt not undoing modifications before making new ones
    ~ fixed 2-opt indexes

--- TSPBench.R
+++ TSPBench.R
@@ -29,22 +29,63 @@ tourLen <- function(data2){
   return(somme)
 }
 
+# applique k_opt et retourne vrai s'il trouve un meilleur tour, faux sinon
+k_opt <- function(x, k=2){
+  or <- function(array){
+    for(i in array){
+      if(i){
+        return(TRUE)
+      }
+    }
+    return(FALSE)
+  }
+  
+  improvement <- FALSE
+  initialIndexValues <- 2:(k+1)
+  i<-initialIndexValues
+  finalIndexValues <- (length(x)-(k-1)):length(x)
+  while(or(i != finalIndexValues)){
+    x_copy <- x
+    temp <- x_copy[i[1], ]
+    for(j in i[-k]){
+      x_copy[i[j], ] <- x_copy[i[j+1], ]
+    }
+    x_copy[i[k], ] <- temp
+    
+    if(tourLen(x_copy) < tourLen(x)){
+      new_x <- x_copy
+      improvement <- TRUE
+    }
+    
+    for(j in k:1){
+      if(i[j] == finalIndexValues[j]){
+        i[j]<-initialIndexValues[j]
+      }else{
+        i[j]<-i[j]+1
+        break()
+      }
+    }
+  }
+  
+  return(improvement)
+}
 
 # applique 2_opt et retourne vrai s'il trouve un meilleur tour, faux sinon
 opt_2 <- function(x){
   improvement <- FALSE
-  x_copy <- data.frame(x)
-  for(i in 2:nrow(x_copy)){
+  x_copy <- x
+  for(i in 2:(nrow(x_copy)-1)){
     for(j in (i+1):nrow(x_copy)){
       temp <- x_copy[i, ]
       x_copy[i, ] <- x_copy[j, ]
       x_copy[j, ] <- temp
       if(tourLen(x_copy) < tourLen(x)){
-        x <- x_copy
+        new_x <- x_copy
         improvement <- TRUE
       }
     }
   }
+  x<-new_x
   return(improvement)
 }
 
