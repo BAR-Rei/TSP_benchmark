5507b986b25740d40c41fc207605d7231b0a4345
Guilhem Piat <guilhempiat@yahoo.fr>
2017-05-20 17:57:16 +0200

    + Added a concorde file creator
    (the one that came prepackaged with R's TSP package created malformed
    files)
    + Added comments

--- TSPBench.R
+++ TSPBench.R
@@ -1,6 +1,21 @@
 install.packages("TSP")
 library(TSP)
+# chemin vers l'executable Concorde
 concorde_path("C:/Program Files (x86)/Concorde")
+
+# ecrit un TSP dans un fichier aux normes Concorde
+toConcordeFile <- function(x, filename = "ConcordeFile.tsp"){
+  dimension = nrow(x)
+  header <- paste("NAME: concorde91
+TYPE: TSP
+DIMENSION:", as.character(dimension),"
+EDGE_WEIGHT_TYPE: EUC_2D
+NODE_COORD_SECTION")
+  write(header, file=filename)
+  for(i in 1:dimension){
+    write(c(i, as.matrix(x[i, ])), file=filename, append = TRUE)
+  }
+}
 
 
 data <- data.frame(x = sample(0:100, 100, replace=T), y = sample(0:100, 100, replace=T), row.names = 1:100)
@@ -17,8 +32,9 @@ tour <- solve_TSP(etsp)
 tour
 plot(etsp, tour, tour_col = "red")
 
-
+toConcordeFile(data)
 
 #distance euclidienne
 dist(data, method = "euclidean", diag = T, upper = FALSE, p = 2)
 dist
+
