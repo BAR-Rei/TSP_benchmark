95041e356bda5bf95828e9c7fe78ecc75515e608
wafaAissaa <wafaaissa94@gmail.com>
2017-05-20 13:02:45 +0200

    first commit

---
+++ TSPBench.R
@@ -0,0 +1,12 @@
+x <- data.frame(x = runif(20, min=0, max=100), y = runif(20, min=0, max=100), row.names = seq(1,20))
+## create a TSP
+etsp <- ETSP(x)
+etsp
+#as.matrix(etsp)
+## use some methods
+n_of_cities(etsp)
+labels(etsp)
+## plot ETSP and solution
+tour <- solve_TSP(etsp)
+tour
+plot(etsp, tour, tour_col = "red")
